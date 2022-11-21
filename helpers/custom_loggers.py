8f3fcfe2030e1cc03c85127ec996a864a69d0122
Dmitrijs Grigorjevs <d.grigorjevs@hotmail.com>
2022-11-21 19:09:23 +0100

    changed console log styles

--- helpers/custom_loggers.py
+++ helpers/custom_loggers.py
@@ -1,33 +1,27 @@
 import coloredlogs
 import logging
-import os
-import time
+#import os
+#import time
 import inspect
-import traceback
+#import traceback
 import sys
 
 ######################################## Custom Console Logger ########################################
 STREAM_DEFAULT_LOG_LEVEL = logging.DEBUG
 STREAM_DEFAULT_LOG_NAME = "cli_Log"
 #DEFAULT FIELD STYLES
-STREAM_DEFAULT_FIELD_STYLES = {'lineno': {'color': 'cyan'}, 'name': {'color': 'black'}, 'levelname': {'color': 'black', 'bold': True, 'bright': True},'funcName': {'color': 'black'}, 'asctime': {'color': 'green'}, 'message': {'color': 'white'}, 'filename': {'color': 'black'},'module': {'color': 'blue'}, 'relativeCreated': {'color': 'green'}, 'msecs': {'color': 'green'}}
+STREAM_DEFAULT_FIELD_STYLES = {'lineno': {'color': 127}, 'name': {'color': 'black'}, 'levelname': {'color': 195, 'bold': True},'funcName': {'color': 'black'}, 'asctime': {'color': 'black', 'bold': True}, 'message': {'color': 'white'}, 'filename': {'color': 'black'},'module': {'color': 'black'}, 'relativeCreated': {'color': 'green'}, 'msecs': {'color': 'green'}}
 # FIELD STYLES FOR ALERT LEVEL
 STREAM_ALERT_FIELD_STYLES = {'lineno': {'color': 'red'}, 'name': {'color': 'black'}, 'levelname': {'color': 'black', 'bold': True, 'bright': True},'funcName': {'color': 'black'}, 'asctime': {'color': 'green'}, 'message': {'color': 'white'}, 'filename': {'color': 'black'},'module': {'color': 'blue'}, 'relativeCreated': {'color': 'green'}, 'msecs': {'color': 'green'}}
 
 # DEFAULT LEVEL STYLES
-STREAM_DEFAULT_LEVEL_STYLES = {'info': {'color': 40, 'bold': True}, 'warning': {'color': 'yellow', 'bold': True}, 'error': {'color': 196, 'bold': True}, 'debug': {'color': 51,'bald': True}, 'critical': {'color': 'white', 'bold': True, 'background': 'red'},'exception': {'color': 'cyan', 'bold': True}, 'alert': {'color': 166, 'bold': True}}
+STREAM_DEFAULT_LEVEL_STYLES = {'info': {'color': 'green', 'bold': False}, 'warning': {'color': 'yellow', 'bold': True}, 'error': {'color': 196, 'bold': False}, 'debug': {'color': 27,'bald': True}, 'critical': {'color': 'white', 'bold': True, 'background': 'red'},'exception': {'color': 196, 'bold': True}, 'alert': {'color': 166, 'bold': True}, 'important': {'color': 40, 'bold': True}}
 # DEFAULT COMSOLE LOG FORMAT
-STREAM_LOG_DEFAULT_FORMAT = '|%(asctime)s|%(name)s|%(levelname)s|   %(message)s   |%(filename)s|%(funcName)s|%(lineno)d|%(module)s|%(relativeCreated)d|%(msecs)d|'
-# FORMAT FOR WARNING LEVEL
-STREAM_LOG_WARNING_FORMAT = '|%(asctime)s|%(name)s|%(levelname)s|   %(message)s   | %(filename)s, %(funcName)s, %(lineno)d, %(module)s, %(relativeCreated)d, %(msecs)d|, %(message)s|'
+STREAM_LOG_DEFAULT_FORMAT = '|%(asctime)s|%(levelname)s|   %(message)s   |%(filename)s|%(funcName)s|%(lineno)d|%(name)s|' #%(module)s|
 # FORMAT FOR ALERT LEVEL
-STREAM_LOG_ALERT_FORMAT = '|%(asctime)s|%(name)s|%(levelname)s|   %(message)s   |'
-# FORMAT FOR IMPORTANT LEVEL
-STREAM_LOG_IMPORTANT_FORMAT = '|%(asctime)s|%(name)s|%(levelname)s|   %(message)s   |'
-# FORMAT FOR EXCEPTION LEVEL
-STREAM_LOG_EXCEPTION_FORMAT = '|%(asctime)s|%(name)s|%(levelname)s|   %(message)s   |'
-# FORMAT FOR ERROR LEVEL
-STREAM_LOG_ERROR_FORMAT = '|%(asctime)s|%(name)s|%(levelname)s|   %(message)s   |, %(lineno)d, %(lineno)d'
+STREAM_LOG_ALERT_FORMAT = '|%(asctime)s|%(levelname)s|   %(message)s'
+STREAM_LOG_IMPORTANT_FORMAT = '|%(asctime)s|%(levelname)s|   %(message)s'
+
 STREAM_DEFAULT_TIME_FORMAT = '%H:%M:%S.%f'
 
 ######################################## Custom File Logger ########################################
@@ -64,14 +58,50 @@ class ConsoleLogger():
         self.logger.propagate = False
 
 
+    # set colored formatter to default format
+    def set_default_formatter(self):
+        self.stream_formatter = coloredlogs.ColoredFormatter(fmt=STREAM_LOG_DEFAULT_FORMAT, field_styles=self.field_styles, level_styles=self.level_styles, datefmt=self.time_format)
+        self.stream_handler.setFormatter(self.stream_formatter)
+    # set new format for colored formatter for specific level
+    def set_colored_formatter_format(self, log_format):
+        self.log_format = log_format
+        self.stream_formatter = coloredlogs.ColoredFormatter(fmt=self.log_format, field_styles=self.field_styles, level_styles=self.level_styles, datefmt=self.time_format)
+        self.stream_handler.setFormatter(self.stream_formatter)
+
+    
+    # set new formatter for specific level
+    def set_formatter(self, level, log_format, time_format):
+        self.log_format = log_format
+        self.time_format = time_format
+        self.stream_formatter = logging.Formatter(fmt=self.log_format, datefmt=self.time_format)
+        self.stream_handler.setFormatter(self.stream_formatter)
+        self.logger.setLevel(level)
+        self.stream_handler.setLevel(level)
+    
+    # set new formatter for all levels
+    def set_formatter_all(self, log_format, time_format):
+        self.log_format = log_format
+        self.time_format = time_format
+        self.stream_formatter = logging.Formatter(fmt=self.log_format, datefmt=self.time_format)
+        self.stream_handler.setFormatter(self.stream_formatter)
+    
+    # set new log level
+    def set_level(self, level):
+        self.logger.setLevel(level)
+        self.stream_handler.setLevel(level)
+    
+    # set new log name
+    def set_name(self, name):
+        self.log_name = name
+        self.logger = logging.getLogger(self.log_name)
+    
+
     # creates new record for logger with file name, function name, line number, message, level etc
     # addes exception type error to record if exception is passed
     def create_record(self, msg, level, exception=None):
         stack = inspect.stack()
         record = logging.LogRecord(name=self.log_name, level=level, pathname=stack[2][1], lineno=stack[2][2], msg=msg, args=None, exc_info=exception, func=stack[2][3])
         return record
-
-
 
     def info(self, msg, *args, **kwargs):
         record = self.create_record(msg, logging.INFO)
@@ -93,13 +123,11 @@ class ConsoleLogger():
         record = self.create_record(msg, logging.CRITICAL)
         self.logger.handle(record)
 
-    def alert(self, msg, *args, **kwargs):
-        record = self.create_record(level=100, msg=msg)
-        self.logger.handle(record)
-
     def important(self, msg, *args, **kwargs):
         record = self.create_record(level=200, msg=msg)
+        self.set_colored_formatter_format(STREAM_LOG_IMPORTANT_FORMAT)
         self.logger.handle(record)
+        self.set_default_formatter()
     
     # exception level is used for logging exceptions
     # prints exception type, exception message
@@ -107,7 +135,14 @@ class ConsoleLogger():
         record = self.create_record(level=300, msg=msg, exception=sys.exc_info())
         self.logger.handle(record)
     
-
+    # alert level is used for logging alerts with different style format
+    def alert(self, msg, *args, **kwargs):
+        record = self.create_record(level=100, msg=msg)
+        # change output format for alert level
+        self.set_colored_formatter_format(STREAM_LOG_ALERT_FORMAT)
+        self.logger.handle(record)
+        # change output format back to default
+        self.set_default_formatter()
     
 
 
